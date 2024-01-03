9497792f9d70e4f0a655a3b334316db82547226c
Jose Lopez <joselopez@Joses-MBP.lan>
2024-01-03 14:38:20 -0500

    fixed key and created the final offsets

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,6 +11,8 @@ class Enigma
     end
     
     def encrypt(message,key=@random_key ,date=@date)
+        final_shifts = shifts(key, date)
+        binding.pry
         hash = {
             message: message,
             key: key,
@@ -24,7 +26,7 @@ class Enigma
 
     def key_generator
         key = ""
-        until key.length == 6
+        until key.length == 5
             random_number = rand(0..9)
             key += random_number.to_s
         end
@@ -36,5 +38,20 @@ class Enigma
         time.slice!(4)
         time.slice!(4)
         time
+    end
+
+    def offset(date)
+        num = date.to_i * date.to_i
+        num.to_s.split("").pop(4)
+    end
+
+    def shifts(key,date)
+        offsets = offset(date)
+        final_shifts = {}
+        final_shifts[:A] = (key[0..1].to_i + offsets[0].to_i)
+        final_shifts[:B] = (key[1..2].to_i + offsets[1].to_i)
+        final_shifts[:C] = (key[2..3].to_i + offsets[2].to_i)
+        final_shifts[:D] = (key[3..4].to_i + offsets[3].to_i)
+        final_shifts
     end
 end 
